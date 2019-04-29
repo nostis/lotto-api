9bfaf4ededaf32e2179c4f4d06b67c0b3f1780f6
nostis <norbert_2000@o2.pl>
2019-04-29 21:12:22 +0200

    Initial commit

---
+++ src/main/java/com/nostis/lottoapi/LottoApiApplication.java
@@ -0,0 +1,11 @@
+package com.nostis.lottoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LottoApiApplication.class, args);
+	}
+}
