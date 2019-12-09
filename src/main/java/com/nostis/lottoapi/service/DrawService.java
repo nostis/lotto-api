bd39feb13d47c0ab14400e6057773258dc92f104
Norbert <norbert_2000@o2.pl>
2019-12-09 19:43:40 +0100

    Remove unnecessary method

--- src/main/java/com/nostis/lottoapi/service/DrawService.java
+++ src/main/java/com/nostis/lottoapi/service/DrawService.java
@@ -7,19 +7,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @Service
 public class DrawService {
     @Autowired
     private DrawRepository drawRepository;
-
-    public List<Draw> getAllDraws(){
-        Iterable<Draw> draws = this.drawRepository.findAll();
-
-        return StreamSupport.stream(draws.spliterator(), false ).collect(Collectors.toList());
-    }
 
     public Optional<Draw> findDrawByDrawNumber(Long number){
         return drawRepository.findByDrawNumber(number);
